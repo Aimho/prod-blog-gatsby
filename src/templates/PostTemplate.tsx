fd78d3172f4e0fdf4725c0c17e496f6a21296ed4
AimHo <jjhwhwnsgh@gmail.com>
2020-05-15 09:47:11 +0900

    [update] disqus url log

--- src/templates/PostTemplate.tsx
+++ src/templates/PostTemplate.tsx
@@ -12,6 +12,7 @@ import { PostTemplateProps } from './types';
 
 const PostTemplate: React.FC<PostTemplateProps> = React.memo(props => {
     const [fadeIn, setFadeIn] = useState(undefined);
+    console.log(`${process.env.BASE_URL}${props.path}`);
 
     const { createdAt, title, tags, html } = props.pageContext;
     const disqusConfig = {
