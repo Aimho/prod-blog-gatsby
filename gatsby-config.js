1969b58904ac83b9b2e1cd5467b6819ae2d43b1c
AimHo <jjhwhwnsgh@gmail.com>
2020-05-19 09:54:00 +0900

    [add] rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,6 +95,59 @@ module.exports = {
                 siteSpeedSampleRate: 10,
             },
         },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                {
+                  site {
+                    siteMetadata {
+                      title
+                      description
+                      siteUrl
+                      site_url: siteUrl
+                    }
+                  }
+                }
+              `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.excerpt,
+                                    date: edge.node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{ 'content:encoded': edge.node.html }],
+                                });
+                            });
+                        },
+                        query: `
+                    {
+                      allMarkdownRemark(
+                        sort: { order: DESC, fields: [frontmatter___date] },
+                      ) {
+                        edges {
+                          node {
+                            excerpt
+                            html
+                            fields { slug }
+                            frontmatter {
+                              title
+                              date
+                            }
+                          }
+                        }
+                      }
+                    }
+                  `,
+                        output: '/rss.xml',
+                        title: "Your Site's RSS Feed",
+                    },
+                ],
+            },
+        },
         `gatsby-plugin-typescript`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sharp`,
@@ -102,8 +155,5 @@ module.exports = {
         `gatsby-plugin-styled-components`,
         `gatsby-plugin-material-ui`,
         `gatsby-plugin-advanced-sitemap`,
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
     ],
 };
